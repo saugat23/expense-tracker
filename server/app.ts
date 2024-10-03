8b8a120e61b5d23a6ac9c06db4fa89e3c4c8fc69
saugat23 <saugatbhandari8@gmail.com>
2024-10-03 13:16:24 +0545

    server setup

---
+++ server/app.ts
@@ -0,0 +1,16 @@
+import { Hono } from "hono";
+import { logger } from "hono/logger";
+import { expensesRoute } from "./routes/expenses";
+
+const app = new Hono();
+
+app.use("*", logger());
+
+app.get("/test", (c) => {
+  return c.json({
+    message: "Hello from Hono",
+  });
+});
+
+app.route("/api/expenses", expensesRoute);
+export default app;
